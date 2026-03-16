de4f7c83edaa53b184206aef995e0f5097e3121a
Paul Diokine Batista <batigaol17@gmail.com>
2026-03-16 20:05:47 +0000

    Add files via upload

---
+++ NLP-project/Big Data & NLP/twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "942057109277102080-OtfShDb3M96e8nvVOGra2CRLHMa50eE"
+ACCESS_TOKEN_SECRET = "rTtOjGne4zO1fkd1B5ENutQ1AivNPHqcOzG43NKUwpVrb"
+CONSUMER_KEY = "y57lwcnGJ127vMRTVULSVrD2N"
+CONSUMER_SECRET = "t9VyBXTrI1RNkYMx9M4ap9fLxxned0wJnHYxJjPMvb8miRrlDp"
